f14025ff4a5417394869b61e2385f344a18892a0
Nebenmor <nebenmor.anthony@gmail.com>
2025-04-24 09:21:42 +0100

    implement 'Who We Are Section'

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
-
-var swiper = new Swiper(".mySwiper", {
+// Gallery section
+var swiper = new Swiper(".gallery", {
   effect: "coverflow",
   grabCursor: true,
   centeredSlides: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
   slidesPerView: "auto",
   coverflowEffect: {
     rotate: 50,
@@ -12,3 +20,27 @@ var swiper = new Swiper(".mySwiper", {
     slideShadows: true,
   },
 });
+
+// Testimonial Section
+var swiper = new Swiper(".testimonial", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  pagination: {
+    el: ".testimonial-pagination",
+    clickable: true,
+  },
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+});
+
+// Date
+const date = document.getElementById("date");
+newDate = new Date().getFullYear();
+date.innerHTML = newDate;
