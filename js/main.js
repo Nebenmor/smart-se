7381e1e5bed0b02a59c8cfe34e4ce2391804ecbf
Nebenmor <nebenmor.anthony@gmail.com>
2025-04-28 10:04:58 +0100

    implement hamburger menu JS

---
+++ js/main.js
@@ -0,0 +1,35 @@
+// Hamburger Menu
+const btn = document.getElementById("menu-icon");
+const nav = document.getElementById("menu");
+
+btn.addEventListener("click", () => {
+  btn.classList.toggle("open");
+  nav.classList.toggle("flex");
+  nav.classList.toggle("hidden");
+
+  console.log("Hey, I got clicked!");
+});
+
+// Gallery section
+var swiper = new Swiper(".gallery", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+});
+
